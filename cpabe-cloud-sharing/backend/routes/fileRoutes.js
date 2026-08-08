d9c1bd4ea726e33fd6d5942c8c216bfeefa61f83
Solomon Daniel Itopa <125440209+Itop19@users.noreply.github.com>
2026-08-08 13:47:20 +0100

    Fix file routes syntax

--- cpabe-cloud-sharing/backend/routes/fileRoutes.js
+++ cpabe-cloud-sharing/backend/routes/fileRoutes.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const { upload, uploadFile, listFiles, downloadFile, decryptFileRoute } = require('../controllers/fileController');
+
+const {
+  upload,
+  uploadFile,
+  listFiles,
+  downloadFile,
+  decryptFileRoute
+} = require('../controllers/fileController');
+
 const { authenticate } = require('../utils/auth');
 
 const router = express.Router();
+
 router.post('/upload', authenticate, upload.single('file'), uploadFile);
+
 router.get('/', authenticate, listFiles);
+
 router.get('/:id/download', authenticate, downloadFile);
+
 router.post('/decrypt', authenticate, decryptFileRoute);
 
 module.exports = router;
